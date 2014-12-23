d10b97c3722328595a773c1383440757578359b3
CodingHouse <sb@codinghouse.co>
2014-12-23 13:42:11 -0800

    Random bg colors

--- simple-todos.js
+++ simple-todos.js
@@ -3,6 +3,7 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
 
   Template.body.helpers({
+    color: function() { return Session.get("color"); },
     days: function () { return Session.get("days"); },
     hours: function () { return Session.get("hours"); },
     minutes: function () { return Session.get("minutes"); },
@@ -17,6 +18,7 @@ if (Meteor.isClient) {
     Session.set("hours", duration.hours());
     Session.set("minutes", duration.minutes());
     Session.set("seconds", duration.seconds());
+    Session.set("color", "#"+(~~(Math.random()*(1<<24))).toString(16));
   }
 
   // var r=4+parseInt(Math.random()*16);for(var i=r; i--;){setTimeout('createFirework(8,14,2,null,null,null,null,null,Math.random()>0.5,true)',(i+1)*(1+parseInt(Math.random()*1000)));}
